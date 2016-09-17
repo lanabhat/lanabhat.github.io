d8df7d675a0142a56513ef7efa244d522d57eeab
lanabhat <lanabhat@gmail.com>
2016-09-17 01:48:06 -0500

    Yakshadhwani Update Management
    This file will be used to add new content and update the user that a new version is available and they can sync with web.

---
+++ updateService.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular.module('YakshadhwaniApp').factory('updateSer', [updateSer]);
+function updateSer() {  
+		var latestVersion = "1.1"
+
+		function CheckVersion(curVer)
+		{
+			
+		}
+
+	 }; //Service finction
+})(); //Self invocation
+
